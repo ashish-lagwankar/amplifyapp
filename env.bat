43bd7b5310c736f3952f8118bf244fdd291ce538
Ashish Lagwankar <lgwnk@amazon.com>
2023-04-11 11:29:05 -0400

    copy of ref material

---
+++ env.bat
@@ -0,0 +1,7 @@
+SET GITHUB_ORG=abirismyname
+SET GITHUB_REPO=amplify-ecs-fargate
+SET ROLE_NAME=hackathon_role
+SET ECR_REPO=hackathon_repo
+SET AWS_REGION=us-east-1
+SET AVAILABILITY_ZONES=us-east-1a,us-east-1b,us-east-1c
+SET ECS_CLUSTER_NAME=hackathon_cluster
